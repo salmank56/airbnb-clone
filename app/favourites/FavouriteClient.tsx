ef8d36cb2915a2ce034b46bb2bbfdaf63721e853
Salman Khan <salmank4954@gmail.com>
2023-07-14 12:17:20 -0700

    checkin

--- app/favourites/FavouriteClient.tsx
+++ app/favourites/FavouriteClient.tsx
@@ -1,12 +1,12 @@
-import { SafeListing, SafeUser } from "@/app/types";
+import { safeListings, safaUser } from "@/app/types";
 
 import Heading from "@/app/components/Heading";
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 
 interface FavoritesClientProps {
-  listings: SafeListing[];
-  currentUser?: SafeUser | null;
+  listings: safeListings[];
+  currentUser?: safaUser | null;
 }
 
 const FavoritesClient: React.FC<FavoritesClientProps> = ({
